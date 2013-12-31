a7a129cf51c858d614ef7049ef972aa841389570
kevinbuch <kevaustinbuch@gmail.com>
2013-12-31 16:53:37 -0600

    moved config to core, added initalize-config

--- conveyor/spec/conveyor/core_spec.clj
+++ conveyor/spec/conveyor/core_spec.clj
@@ -2,10 +2,174 @@
   (:require [speclj.core :refer :all]
             [clojure.java.io :refer [file]]
             [ring.mock.request :as mr]
-            [conveyor.config :refer :all]
             [conveyor.core :refer :all]
             [conveyor.precompile :refer [precompile]])
   (:import [org.apache.commons.io FileUtils]))
+
+(describe "conveyor.config"
+
+  (it "initializes the config"
+    (should (initialize-config {})))
+
+  (context "resource-directory-path"
+
+    (it "builds the full path to a jar resource directory"
+      (let [full-path (resource-directory-path "stylesheets" "test1.css")]
+        (should= ".test1 { color: white; }\n" (slurp (str full-path "/test1.css")))))
+
+    (it "returns nil when the directory does not exist"
+      (should-be-nil (resource-directory-path "non_existant_dir" "test1.css")))
+
+    (it "returns nil when given resources does not exist"
+      (should-be-nil (resource-directory-path "stylesheets" "unknown")))
+
+    )
+
+  (context "directory-path"
+
+    (it "builds the full path to a directory"
+      (let [full-path (directory-path "test_fixtures/public/stylesheets")]
+        (should= ".test2 { color: black; }\n" (slurp (str full-path "/test2.css")))))
+
+    (it "returns nil when the directory does not exist"
+      (should-be-nil (directory-path "non_existant_dir")))
+
+    )
+
+  (context "add resource directory to load path"
+
+    (it "adds valid resource directory to the load path"
+      (let [full-path (resource-directory-path "stylesheets" "test1.css")
+            new-config (add-resource-directory-to-load-path {:load-paths []} "stylesheets" "test1.css")]
+        (should= [full-path] (:load-paths new-config))))
+
+    (it "throws an exception when the resource directory does not exist"
+      (should-throw IllegalArgumentException "Could not find resource directory: uknown-dir"
+                    (add-resource-directory-to-load-path {:load-paths []} "uknown-dir" "test1.css")))
+
+    )
+
+  (context "add directory to load path"
+
+    (it "adds valid directory to the load path"
+      (let [full-path (directory-path "test_fixtures/public/stylesheets")
+            new-config (add-directory-to-load-path {:load-paths []} "test_fixtures/public/stylesheets")]
+        (should= [full-path] (:load-paths new-config))))
+
+    (it "throws an exception when the directory does not exist"
+      (should-throw IllegalArgumentException "Could not find directory: uknown-dir"
+                    (add-directory-to-load-path {:load-paths []} "uknown-dir")))
+
+    )
+
+  (context "initialize-config"
+
+    (it "adds a resource directory path to the load path"
+      (let [full-path (resource-directory-path "stylesheets" "test1.css")
+            config (initialize-config {:load-paths [{:type :resource-directory
+                                                            :path "stylesheets"
+                                                            :file-in-dir "test1.css"}]})]
+        (should= [full-path] (:load-paths config))))
+
+    (it "adds a directory path to the load path"
+      (let [full-path (directory-path "test_fixtures/public/stylesheets")
+            config (initialize-config {:load-paths [{:type :directory
+                                                            :path "test_fixtures/public/stylesheets"}]})]
+        (should= [full-path] (:load-paths config))))
+
+    (it "throws an exception for an unknown load path type"
+      (should-throw
+        Exception
+        "Unknown type of load-path: :unknown-type. Valid types are :resource-directory and :directory."
+        (initialize-config {:load-paths [{:type :unknown-type}]})))
+
+    (it "configures the prefix"
+      (let [config (initialize-config {:prefix "/assets"})]
+        (should= "/assets" (:prefix config))))
+
+    (it "configures a plugin"
+      (let [full-path (directory-path "test_fixtures/public/stylesheets")
+            config (initialize-config {:plugins [:test-plugin]})]
+        (should= "configured" (:test-config config))
+        (should= [full-path] (:load-paths config))))
+
+    (it "configures multiple plugins"
+      (let [style-path (directory-path "test_fixtures/public/stylesheets")
+            js-path (directory-path "test_fixtures/public/javascripts")
+            config (initialize-config {:plugins [:test-plugin :other-plugin]})]
+        (should= "configured" (:test-config config))
+        (should== [style-path js-path] (:load-paths config))))
+
+    (it "configures a plugin that takes options"
+      (let [config (initialize-config {:plugins [{:plugin-name :option-plugin :option1 2}]})]
+        (should= 2 (:option1 config))))
+
+    (it "configures the asset host"
+      (let [config (initialize-config {:asset-host "test-host"})]
+        (should= "test-host" (:asset-host config))))
+
+    (it "configures the use-digest-path"
+      (let [config (initialize-config {:use-digest-path true})]
+        (should (:use-digest-path config))))
+
+    (it "configures the output-dir"
+      (let [config (initialize-config {:output-dir "test_output"})]
+        (should= "test_output" (:output-dir config))))
+
+    (it "uses public as the default the output-dir"
+      (let [config (initialize-config {:output-dir nil})]
+        (should= "public" (:output-dir config))))
+
+    (it "configures the manifest"
+      (let [config (initialize-config {:manifest "some-other-manifest.edn"})]
+        (should= "some-other-manifest.edn" (:manifest config))))
+
+    (it "configures the search strategy"
+      (let [config (initialize-config {:asset-finder :precompiled})]
+        (should= :precompiled (:asset-finder config))))
+
+    (it "defaults the search strategy to load-path"
+      (let [config (initialize-config {})]
+        (should= :load-path (:asset-finder config))))
+
+    (it "sets compression to true"
+      (let [config (initialize-config {:compress true})]
+        (should= true (:compress config))))
+
+    (it "sets compression to false"
+      (let [config (initialize-config {:compress false})]
+        (should= false (:compress config))))
+
+    (it "defaults compression to false"
+      (let [config (initialize-config {})]
+        (should= false (:compress config))))
+
+    (it "sets compile to true"
+      (let [config (initialize-config {:compile true})]
+        (should= true (:compile config))))
+
+    (it "sets compile to false"
+      (let [config (initialize-config {:compile false})]
+        (should= false (:compile config))))
+
+    (it "defaults compile to true"
+      (let [config (initialize-config {})]
+        (should= true (:compile config))))
+
+    (it "sets pipeline-enabled to true"
+      (let [config (initialize-config {:pipeline-enabled true})]
+        (should= true (:pipeline-enabled config))))
+
+    (it "sets pipeline-enabled to true"
+      (let [config (initialize-config {:pipeline-enabled false})]
+        (should= false (:pipeline-enabled config))))
+
+    (it "defaults pipeline-enabled to true"
+      (let [config (initialize-config {})]
+        (should= true (:pipeline-enabled config))))
+
+    )
+  )
 
 (describe "conveyor.core"
 
--- conveyor/src/conveyor/core.clj
+++ conveyor/src/conveyor/core.clj
@@ -1,11 +1,141 @@
 (ns conveyor.core
   (:require [clojure.string :as clj-str]
+            [clojure.java.io :refer [resource file]]
             [conveyor.file-utils :refer [file-join]]
             [conveyor.compile :refer [compile-asset]]
             [conveyor.compress :refer [compress-asset]]
-            [conveyor.config :refer [compile? compress?]]
             [conveyor.finder.interface :refer [get-asset get-logical-path get-digest-path]]
             [conveyor.finder.factory :refer [make-asset-finder]]))
+
+(defn compile? [config]
+  (and (:pipeline-enabled config) (:compile config)))
+
+(defn compress? [config]
+  (and (:pipeline-enabled config) (:compress config)))
+
+(defn- base-dir [full-path sub-path]
+  (first (clj-str/split full-path (re-pattern sub-path) 2)))
+
+(defn- append-to-key [m key value]
+  (update-in m [key] #(conj % value)))
+
+(defn add-compiler-config [config compiler-config]
+  (append-to-key config :compilers compiler-config))
+
+(defn add-compressor-config [config compressor-config]
+  (append-to-key config :compressors compressor-config))
+
+(defn add-output-extension [config extension]
+  (append-to-key config :output-extensions extension))
+
+(defn add-input-extension [config extension]
+  (append-to-key config :input-extensions extension))
+
+(defn set-input-extension [config extension]
+  (assoc config :input-extension extension))
+
+(defn set-compiler [config compiler]
+  (assoc config :compiler compiler))
+
+(defn set-compressor [config compressor]
+  (assoc config :compressor compressor))
+
+(def default-compiler-config
+  {:input-extensions []
+   :output-extensions []
+   :compiler (fn [config asset input-extension output-extension] asset)})
+
+(defmacro configure-compiler [& body]
+  `(-> default-compiler-config
+     ~@body))
+
+(def default-compressor-config
+  {:input-extension nil
+   :compressor (fn [config body filename] body)})
+
+(defmacro configure-compressor [& body]
+  `(-> default-compressor-config
+     ~@body))
+
+(defn add-prefix [config prefix]
+  (assoc config :prefix prefix))
+
+(defn set-use-digest-path [config value]
+  (assoc config :use-digest-path value))
+
+(defn set-output-dir [config path]
+  (assoc config :output-dir path))
+
+(defn set-asset-finder [config strat]
+  (assoc config :asset-finder strat))
+
+(defn- normalize-asset-host [host]
+  (when host
+    (if (.endsWith host "/")
+      (.substring host 0 (dec (count host)))
+      host)))
+
+(defn set-asset-host [config host]
+  (assoc config :asset-host (normalize-asset-host host)))
+
+(defn set-manifest [config manifest-path]
+  (assoc config :manifest manifest-path))
+
+(defn set-compression [config compression]
+  (assoc config :compress compression))
+
+(defn set-compile [config compile]
+  (assoc config :compile compile))
+
+(defn set-pipeline-enabled [config enabled]
+  (assoc config :pipeline-enabled enabled))
+
+(defmacro thread-pipeline-config [& body]
+  `(-> default-pipeline-config
+     ~@body))
+
+(defn- configure-prefix [config {:keys [prefix]}]
+  (if prefix
+    (add-prefix config prefix)
+    config))
+
+;(defn- configure-asset-host [config {:keys [asset-host]}]
+;  (set-asset-host config asset-host))
+;
+;(defn- configure-use-digest-path [config {:keys [use-digest-path]}]
+;  (set-use-digest-path config use-digest-path))
+;
+;(defn- configure-output-dir [config {:keys [output-dir]}]
+;  (set-output-dir config (or output-dir (:output-dir config))))
+;
+;(defn- configure-manifest [config {:keys [manifest]}]
+;  (set-manifest config manifest))
+;
+;(defn- configure-asset-finder [config {:keys [asset-finder]}]
+;  (set-asset-finder config (or asset-finder (:asset-finder config))))
+;
+;(defn- configure-compression [config {:keys [compress]}]
+;  (set-compression config (if (nil? compress) (:compress config) compress)))
+;
+;(defn- configure-compile [config {:keys [compile]}]
+;  (set-compile config (if (nil? compile) (:compile config) compile)))
+;
+;(defn- configure-pipeline [config {:keys [pipeline-enabled]}]
+;  (set-pipeline-enabled config (if (nil? pipeline-enabled) (:pipeline-enabled config) pipeline-enabled)))
+;
+;(defn configure-asset-pipeline [config]
+;  (thread-pipeline-config
+;    (configure-load-paths config)
+;    (configure-prefix config)
+;    (configure-plugins config)
+;    (configure-asset-host config)
+;    (configure-use-digest-path config)
+;    (configure-output-dir config)
+;    (configure-manifest config)
+;    (configure-asset-finder config)
+;    (configure-compression config)
+;    (configure-compile config)
+;    (configure-pipeline config)))
 
 (declare ^:dynamic *pipeline*)
 (declare ^:dynamic *pipeline-config*)
@@ -69,6 +199,91 @@
   (let [{:keys [finder pipeline-fn]} (pipeline)]
     (when-let [asset (get-asset finder path)]
       (pipeline-fn path asset))))
+
+(defn directory-path [path]
+  (let [directory (file path)]
+    (when (.exists directory)
+      (.getAbsolutePath directory))))
+
+(defn- normalize-resource-url [url]
+  (if (= "file" (.getProtocol url))
+    (directory-path (.getPath url))
+    (str url "/")))
+
+(defn resource-directory-path [directory-path resource-in-directory]
+  (let [with-leading-slash (str "/" resource-in-directory)
+        relative-path (str directory-path with-leading-slash)]
+    (when-let [resource-url (resource relative-path)]
+      (base-dir (normalize-resource-url resource-url) with-leading-slash))))
+
+(defn add-to-load-path [config path]
+  (append-to-key config :load-paths path))
+
+(defn add-resource-directory-to-load-path [config directory-path resource-in-directory]
+  (if-let [full-path (resource-directory-path directory-path resource-in-directory)]
+    (add-to-load-path config full-path)
+    (throw (IllegalArgumentException. (str "Could not find resource directory: " directory-path)))))
+
+(defn add-directory-to-load-path [config path]
+  (if-let [full-path (directory-path path)]
+    (add-to-load-path config full-path)
+    (throw (IllegalArgumentException. (str "Could not find directory: " path)))))
+
+(defn- throw-unknown-load-path-type [type]
+  (throw
+    (Exception.
+      (format
+        "Unknown type of load-path: %s. Valid types are :resource-directory and :directory."
+        type))))
+
+(defn- configure-load-paths [{:keys [load-paths] :as config}]
+  (reduce
+    (fn [config {:keys [type path file-in-dir]}]
+      (cond
+        (= :resource-directory type)
+        (add-resource-directory-to-load-path config path file-in-dir)
+        (= :directory type)
+        (add-directory-to-load-path config path)
+        :else
+        (throw-unknown-load-path-type type)))
+    (assoc config :load-paths [])
+    load-paths))
+
+(defn- configure-plugins [{:keys [plugins] :as config}]
+  (reduce
+    (fn [config plugin]
+      (let [[plugin-name options] (if (map? plugin)
+                                    [(:plugin-name plugin) (dissoc plugin :plugin-name)]
+                                    [plugin nil])
+            ns-sym (symbol (format "conveyor.%s" (name plugin-name)))]
+        (require ns-sym)
+        (let [configure-sym (symbol (format "configure-%s" (name plugin-name)))
+              configure-fn (ns-resolve (the-ns ns-sym) configure-sym)]
+          (if options
+            (configure-fn config options)
+            (configure-fn config)))))
+    config
+    plugins))
+
+(def default-pipeline-config
+  {:load-paths []
+   :compilers []
+   :compressors []
+   :prefix "/"
+   :output-dir "public"
+   :asset-finder :load-path
+   :compress false
+   :compile true
+   :pipeline-enabled true})
+
+(defn apply-defaults [config]
+  (merge-with #(if (nil? %2) %1 %2) default-pipeline-config config))
+
+(defn initialize-config [config]
+  (-> config
+      apply-defaults
+      configure-load-paths
+      configure-plugins))
 
 (defmacro bind-config [config pipeline & body]
   `(binding [*pipeline-config* ~config

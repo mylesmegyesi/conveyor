9303fc91d07632399b3a7fa1b119f8030a72526d
Myles Megyesi <mylesmegyesi@me.com>
2014-01-20 10:32:48 -0600

    bump version

--- conveyor/project.clj
+++ conveyor/project.clj
@@ -1,4 +1,4 @@
-(defproject conveyor "0.2.6"
+(defproject conveyor "0.2.7"
   :description "An implementation of the Rails asset pipeline for Clojure"
   :url         "https://github.com/mylesmegyesi/conveyor"
   :license     {:name "Eclipse Public License"
